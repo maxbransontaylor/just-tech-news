f1c94026d1a5bdb60a123dc0e87a17c0d09b116d
Max Taylor <maxbransontaylor@gmail.com>
2022-08-29 14:08:21 -0400

    use handlebars to generate home page

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require("express");
-const routes = require("./routes");
+const exphbs = require("express-handlebars");
+const hbs = exphbs.create({});
+const routes = require("./controllers");
+const path = require("path");
 const sequelize = require("./config/connection");
 const app = express();
 require("dotenv").config();
 const PORT = process.env.PORT;
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public")));
 app.use(routes);
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Now listening"));
